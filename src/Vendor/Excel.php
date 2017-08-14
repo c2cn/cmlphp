27b5fc0f6f8ab5054d54c0c07afabc6efcf7ac0c
linhecheng <linhechengbush@live.com>
2017-08-14 15:05:10 +0800

    整合excel view代码调用到vendor下的excel类

--- src/Vendor/Excel.php
+++ src/Vendor/Excel.php
@@ -6,6 +6,7 @@
  * @version  @see \Cml\Cml::VERSION
  * cmlphp框架 Excel生成类
  * *********************************************************** */
+
 namespace Cml\Vendor;
 
 /**
@@ -32,22 +33,22 @@ class Excel
      *
      * @return void
      */
-    public function config($enCoding,$boolean,$title,$filename)
+    public function config($enCoding, $boolean, $title, $filename)
     {
         //编码
         $this->coding = $enCoding;
         //转换类型
-        if ($boolean == true){
+        if ($boolean == true) {
             $this->type = 'Number';
         } else {
             $this->type = 'String';
         }
         //表标题
         $title = preg_replace('/[\\\|:|\/|\?|\*|\[|\]]/', '', $title);
-        $title = substr ($title, 0, 30);
-        $this->tWorksheetTitle=$title;
+        $title = substr($title, 0, 30);
+        $this->tWorksheetTitle = $title;
         //文件名
-        $filename = preg_replace('/[^aA-zZ0-9\_\-]/', '', $filename);
+        //$filename = preg_replace('/[^aA-zZ0-9\_\-]/', '', $filename);
         $this->filename = $filename;
     }
 
@@ -71,14 +72,15 @@ class Excel
     private function addRow($data)
     {
         $cells = '';
-        foreach ($data as $val){
+        foreach ($data as $val) {
             $type = $this->type;
             //字符转换为 HTML 实体
-            $val = htmlentities($val,ENT_COMPAT,$this->coding);
+            $val = htmlentities($val, ENT_COMPAT, $this->coding);
             $cells .= "<Cell><Data ss:Type=\"$type\">" . $val . "</Data></Cell>\n";
         }
         return $cells;
     }
+
     /**
      * 生成Excel文件
      *
@@ -89,17 +91,17 @@ class Excel
     public function excelXls($data)
     {
         header("Content-Type: application/vnd.ms-excel; charset=" . $this->coding);
-        header("Content-Disposition: inline; filename=\"" . $this->filename . ".xls\"");
+        header('Content-Disposition: attachment; filename="' . rawurlencode($this->filename . ".xls") . '"');
         /*打印*/
-        echo stripslashes (sprintf($this->header, $this->coding));
+        echo stripslashes(sprintf($this->header, $this->coding));
         echo "\n<Worksheet ss:Name=\"" . $this->tWorksheetTitle . "\">\n<Table>\n";
 
         if (is_array($this->titleRow)) {
-            echo "<Row>\n".$this->addRow($this->titleRow)."</Row>\n";
+            echo "<Row>\n" . $this->addRow($this->titleRow) . "</Row>\n";
         }
-        foreach ($data as $val){
-            $rows=$this->addRow($val);
-            echo "<Row>\n".$rows."</Row>\n";
+        foreach ($data as $val) {
+            $rows = $this->addRow($val);
+            echo "<Row>\n" . $rows . "</Row>\n";
         }
         echo "</Table>\n</Worksheet>\n";
         echo "</Workbook>";
